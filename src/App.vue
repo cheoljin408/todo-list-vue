62b54c6a29c373a9c24dfe1c2b01c7573124d0ae
cheoljin408 <kcj409@naver.com>
2023-03-20 14:12:55 +0900

    feat: daisy-ui, tailwind css 적용

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="sm:container sm:mx-auto px-10 py-10">
+<!--    <div class="flex justify-center">-->
+<!--      <h1 class="text-3xl font-bold">-->
+<!--        Hello tailwindcss!-->
+<!--      </h1>-->
+<!--    </div>-->
+    <header class="flex justify-center mb-8">
+      <hgroup>
+        <h1 class="text-3xl font-bold">나의 할일</h1>
+        <em>{{ today }}</em>
+      </hgroup>
+    </header>
+    <todo-list-container/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { inject } from "vue";
+import TodoListContainer from "./components/TodoListContainer.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoListContainer,
+  },
+  setup() {
+    const today = inject('today')
+
+    return {
+      today
+    }
+  },
 }
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: [
+        'dark',
+        'light',
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
+}
