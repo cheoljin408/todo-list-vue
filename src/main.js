3391a8c4e7dc425f94079a26a653a6529439218f
cheoljin408 <kcj409@naver.com>
2023-03-20 14:09:14 +0900

    feat: 데이터의 필터링 모듈

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.provide('today', new Date().toISOString().split('T')[0])
+app.mount('#app')
